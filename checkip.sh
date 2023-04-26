5d0fa78757d315d46dc6a229ddd896998cc68ebf
galtjay <444602953@qq.com>
2023-04-26 17:28:41 +0800

    update checkip

--- checkip.sh
+++ checkip.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-ip=$myip #在docker运行时需要进行传递，不传递的话默认会终止程序
+ip=$myip
 while true
-ipnow=`export ALL_PROXY=socks5://127.0.0.1:40000 &&curl ifconfig.me`
+ipnow=`export ALL_PROXY=socks5://127.0.0.1:40000 && curl myip.ipip.net | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'`
 echo "current ip is "$ipnow "expect ip is" $ip >> /var/log/warp.log
 do
   if [ -z "$ip" ]; then
     echo "IP address is not set" >> /var/log/warp.log
     break
-  elif [ $ipnow == $ip ]; then
+  elif [[ "$ipnow" ==  "$ip" ]]; then
     echo "IP address meets the expected state,sleep 10 mins" >> /var/log/warp.log
     sleep 600
   else
@@ -15,4 +15,3 @@ do
     warp-cli disconnect && sleep 3 &&warp-cli connect && sleep 10
   fi
 done
-
--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ sleep 5
 warp-cli --accept-tos register
 warp-cli --accept-tos  set-mode proxy
 warp-cli  --accept-tos connect
-nohup sh ./checkip.sh  &
+nohup ./checkip.sh  &
 tail -f /var/log/warp.log
 
