76cabe124f453e59eb6dcf9983e144233ab2c41e
galtjay <444602953@qq.com>
2023-06-02 10:37:07 +0800

    change a way to check ip

--- checkip.sh
+++ checkip.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 ip=$myip
+sleep 5
 while true
 ipnow=`export ALL_PROXY=socks5://127.0.0.1:40000 && curl myip.ipip.net | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'`
-echo "current ip is "$ipnow "expect ip is" $ip >> /var/log/warp.log
+echo "current ip is "$ipnow "excluded ip is" $ip >> /var/log/warp.log
 do
   if [ -z "$ip" ]; then
     echo "IP address is not set" >> /var/log/warp.log
     break
-  elif [[ "$ipnow" ==  "$ip" ]]; then
+  elif [[ "$ipnow" !=  "$ip" ]]; then
     echo "IP address meets the expected state,sleep 10 mins" >> /var/log/warp.log
     sleep 600
   else
